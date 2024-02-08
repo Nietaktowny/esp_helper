645bc103ca5939d9fe72b64061897eb8a9504405
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2024-02-08 22:02:38 +0100

    add lib cloning script

--- tools/git_clone_library.py
+++ tools/git_clone_library.py
@@ -1,8 +1,34 @@
 #Import("env")
-from git import Repo  # pip install gitpython
+import os
+import tempfile
+import shutil
+import git
+from git import RemoteProgress
+from tqdm import tqdm
 
-git_arguments = input("Enter library name: ")
-#git_arguments = env.GetProjectOption("git_arguments", "")
-git_url = "https://github.com/Nietaktowny/wifi_controller.git"
+user_input = input("Enter libraries name: ")
+git_url = "https://github.com/Nietaktowny/esp_helper.git"
+lib = user_input.split()
 
-Repo.clone_from(git_url, git_arguments)
+class CloneProgress(RemoteProgress):
+    def __init__(self):
+        super().__init__()
+        self.pbar = tqdm()
+
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        self.pbar.total = max_count
+        self.pbar.n = cur_count
+        self.pbar.refresh()
+
+# Clear old versions of libraries
+if os.path.isdir("lib"):
+    shutil.rmtree("lib")
+# Create temporary dir
+t = tempfile.mkdtemp()
+# Clone into temporary dir
+git.Repo.clone_from(git_url, t, progress=CloneProgress())
+# Copy desired files from temporary dir
+for file in lib:
+    shutil.move(os.path.join(t, 'libraries/' + file), 'lib/' + file)
+# Remove temporary dir
+shutil.rmtree(t)
