523237800852e746c42da1198bcce98a34d7bd93
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2024-02-17 22:40:40 +0100

    esp_helper_utils tests

--- projects/lib_tests/test/embedded/test_esp_helper_utils/test_esp_helper_utils.c
+++ projects/lib_tests/test/embedded/test_esp_helper_utils/test_esp_helper_utils.c
@@ -17,23 +17,113 @@ void tearDown(void) {
   // clean stuff up here
 }
 
-void test_if_get_base_mac_returns_not_zero(void) {
+void test_if_helper_prepare_url_prepares_good_url(void) {
   //before
   LOG_FATAL("RUNNING: %s", __func__);
 
   //given
-
+  const char* expected = "http://localhost:8081/esp_helper/ota.php?device_id=111111";
+  const char* ota_url = "http://localhost:8081/esp_helper/ota.php";
+  char url[100] = {0};
+  char device_id[20] = "111111";
 
   //when
-
+  helper_prepare_url_with_device_id(ota_url, device_id, url, sizeof(url));
 
   //then
+  TEST_ASSERT_EQUAL_STRING_MESSAGE(expected, url, "helper_prepare_url_with_device_id returned different url from expected");
+}
 
+void test_if_helper_prepare_url_returns_zero(void) {
+  //before
+  LOG_FATAL("RUNNING: %s", __func__);
+
+  //given
+  int err = -1;
+  const char* ota_url = "http://localhost:8081/esp_helper/ota.php";
+  char url[100] = {0};
+  char device_id[20] = "111111";
+
+  //when
+  err = helper_prepare_url_with_device_id(ota_url, device_id, url, sizeof(url));
+
+  //then
+  TEST_ASSERT_EQUAL_MESSAGE(0, err, "helper_prepare_url_with_device_id should return 0.");
+}
+
+void test_if_helper_prepare_url_returns_err_on_null_ota_url(void) {
+  //before
+  LOG_FATAL("RUNNING: %s", __func__);
+
+  //given
+  int err = 0;
+  char url[100] = {0};
+  const char device_id[20] = "111111";
+
+  //when
+  err = helper_prepare_url_with_device_id(NULL, device_id, url, sizeof(url));
+
+  //then
+  TEST_ASSERT_EQUAL_MESSAGE(ERR_NULL_POINTER, err, "helper_prepare_url_with_device_id should return err on NULL ota URL");
+}
+
+void test_if_helper_prepare_url_returns_err_on_null_device_id(void) {
+  //before
+  LOG_FATAL("RUNNING: %s", __func__);
+
+  //given
+  int err = 0;
+  const char* ota_url = "http://localhost:8081/esp_helper/ota.php";
+  char url[100] = {0};
+
+  //when
+  err = helper_prepare_url_with_device_id(ota_url, NULL, url, sizeof(url));
+
+  //then
+  TEST_ASSERT_EQUAL_MESSAGE(ERR_NULL_POINTER, err, "helper_prepare_url_with_device_id should return err on NULL device_id");
+}
+
+void test_if_helper_prepare_url_returns_err_on_null_buffer(void) {
+  //before
+  LOG_FATAL("RUNNING: %s", __func__);
+
+  //given
+  int err = 0;
+  const char* ota_url = "http://localhost:8081/esp_helper/ota.php";
+  const char device_id[20] = "111111";
+
+  //when
+  err = helper_prepare_url_with_device_id(ota_url, device_id, NULL, 120);
+
+  //then
+  TEST_ASSERT_EQUAL_MESSAGE(ERR_NULL_POINTER, err, "helper_prepare_url_with_device_id should return err on NULL buffer");
+}
+
+void test_if_helper_prepare_url_returns_err_on_too_small_buffer(void) {
+  //before
+  LOG_FATAL("RUNNING: %s", __func__);
+
+  //given
+  int err = 0;
+  const char* ota_url = "http://localhost:8081/esp_helper/ota.php";
+  const char device_id[20] = "111111";
+  char url[100] = {0};
+
+  //when
+  err = helper_prepare_url_with_device_id(ota_url, device_id, url, 50);
+
+  //then
+  TEST_ASSERT_EQUAL_MESSAGE(ERR_C_MEMORY_ERR, err, "helper_prepare_url_with_device_id should return err on too small buffer");
 }
 
 int runUnityTests(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_if_get_base_mac_returns_not_zero);
+  RUN_TEST(test_if_helper_prepare_url_returns_err_on_too_small_buffer);
+  RUN_TEST(test_if_helper_prepare_url_returns_err_on_null_buffer);
+  RUN_TEST(test_if_helper_prepare_url_returns_err_on_null_device_id);
+  RUN_TEST(test_if_helper_prepare_url_prepares_good_url);
+  RUN_TEST(test_if_helper_prepare_url_returns_zero);
+  RUN_TEST(test_if_helper_prepare_url_returns_err_on_null_ota_url);
   return UNITY_END();
 }
 
