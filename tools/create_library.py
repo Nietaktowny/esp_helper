d93d3dcaa0a36b34e17728fd25fef6764beac68a
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2024-02-17 11:15:04 +0100

    Make new libraries esp-idf compliant and add markdown file.

--- tools/create_library.py
+++ tools/create_library.py
@@ -6,12 +6,19 @@ include_dir = "include"
 cmake_file = "CMakeLists.txt"
 idf_component_file = "idf_component.yml"
 library_file = "library.json"
+readme_file = "README.md"
 header_file = lib_name + ".h"
 src_file = lib_name + ".c"
 
-cmake_file_text = "idf_component_register(SRCS '" + src_file +"' INCLUDE_DIRS 'include')"
+readme_file_text = "# " + lib_name + "\n" + lib_description
 
-component_file_text = "dependencies:\n  idf: '>=5.0' \ndescription: " + lib_description + ". \nurl: \nversion: 1.0.0" 
+cmake_file_text = ''''
+idf_component_register(
+    SRCS "src/''' + src_file +'''" 
+    INCLUDE_DIRS "include"
+    PRIV_REQUIRES logger err_controller memory_utils)'''
+
+component_file_text = "dependencies:\n  idf: '>=5.0' \ndescription: " + lib_description + ".\nversion: 1.0.0" 
 
 library_json_text = "{\n\t\"name\": \"" + lib_name + "\",\n\t\"version\": \"1.0.0\",\n\t\"keywords\": \"keyword\",\n\t\"description\": \"" + lib_description + "\",\n\t\"frameworks\": [\"espidf\"],\n\t\"platforms\": [\"espressif32\"]\n}"
 
@@ -39,6 +46,10 @@ os.chdir(new_lib_path)
 
 os.makedirs(src_dir, mode)
 os.makedirs(include_dir, mode)
+
+f = open(readme_file, "w")
+f.write(readme_file_text)
+f.close()
 
 f = open(cmake_file, "w")
 f.write(cmake_file_text)
