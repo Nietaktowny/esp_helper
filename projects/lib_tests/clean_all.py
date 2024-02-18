86ccc020e064d77e8ba0b51461a986d10f60f29a
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2024-02-18 22:30:50 +0100

    Don't use termcolor in clean script.

--- projects/lib_tests/clean_all.py
+++ projects/lib_tests/clean_all.py
@@ -1,11 +1,9 @@
 import os
 import subprocess
-from colorama import init
-from termcolor import colored
 
 for file in os.listdir():
     if file.startswith("test_"):
-        print(colored('Cleaning test directory: ' + file, 'green', 'on_black'))
+        print('Cleaning test directory: ' + file)
         os.chdir(file)
         subprocess.run(["idf.py", "fullclean"])
         os.chdir("..")
