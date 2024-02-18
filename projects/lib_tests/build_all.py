ec555c88e1757fbaf840954e8e213419a1f512d2
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2024-02-18 21:04:15 +0100

    Update build_all.py

--- projects/lib_tests/build_all.py
+++ projects/lib_tests/build_all.py
@@ -1,11 +1,9 @@
 import os
 import subprocess
-from colorama import init
-from termcolor import colored
 
 for file in os.listdir():
     if file.startswith("test_"):
-        print(colored('Building test directory: ' + file, 'green', 'on_black'))
+        print('Building test directory: ' + file)
         os.chdir(file)
         subprocess.run(["idf.py", "build"])
         os.chdir("..")
