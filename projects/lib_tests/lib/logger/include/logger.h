16b16d56307c075d012a7daa1b89ad37925407df
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2024-02-22 13:20:49 +0100

    Add docs, and mutex obtaining check.

--- projects/lib_tests/lib/logger/include/logger.h
+++ projects/lib_tests/lib/logger/include/logger.h
@@ -3,40 +3,49 @@
 #include <stdio.h>
 #include <time.h>
 
+#define LOGGER_ERR_BASE         0x0950                                ///< Logger error base.
+#define LOGGER_ERR_NO_MUTEX     0x0001 + LOGGER_ERR_BASE              ///< When logger mutex was not initialized.
+#define LOGGER_MUTEX_TIMEOUT    0x0002 + LOGGER_ERR_BASE              ///< When function getting semaphore timeout expired and cannot obtain log_mutex.
 
-#define LOG_LEVEL_ZERO  0x00
-#define LOG_LEVEL_FATAL 0x01
-#define LOG_LEVEL_ERROR 0x02
-#define LOG_LEVEL_WARN  0x03
-#define LOG_LEVEL_INFO  0x04
-#define LOG_LEVEL_DEBUG 0x05
+
+#define LOG_LEVEL_ZERO          0x00                                  ///< Log level zero, no logging.
+#define LOG_LEVEL_FATAL         0x01                                  ///< Log level fatal, log only fatal errors.
+#define LOG_LEVEL_ERROR         0x02                                  ///< Log level error, log only errors, and fatal errors.
+#define LOG_LEVEL_WARN          0x03                                  ///< Log level warn, log warnings, errors and fatal errors.
+#define LOG_LEVEL_INFO          0x04                                  ///< Log level info, log info logs, warnings, errors, and fatal errors.
+#define LOG_LEVEL_DEBUG         0x05                                  ///< Log level debug, log debug information as well as all log levels apart from verbose logs.
+#define LOG_LEVEL_VERBOSE       0x06                                  ///< Log level verbose, log all logs.
 
 #ifndef FATAL_TAG
-  #define FATAL_TAG   "FATAL"
+  #define FATAL_TAG   "FATAL"                                         ///< Fatal logs tag. 
 #endif
 #ifndef ERROR_TAG
-  #define ERROR_TAG   "ERROR"
+  #define ERROR_TAG   "ERROR"                                         ///< Error logs tag.
 #endif
 #ifndef WARN_TAG
-  #define WARN_TAG    "WARN"
+  #define WARN_TAG    "WARN"                                          ///< Warning logs tag.
 #endif
 #ifndef INFO_TAG
-  #define INFO_TAG    "INFO"
+  #define INFO_TAG    "INFO"                                          ///< Informational logs tag.
 #endif
 #ifndef DEBUG_TAG
-  #define DEBUG_TAG   "DEBUG"
+  #define DEBUG_TAG   "DEBUG"                                         ///< Debug logs tag.
 #endif
+#ifndef VERBOSE_TAG
+  #define VERBOSE_TAG "VERBOSE"                                       ///< Verbose logs tag.
+#endif
+
                             /*color,time| tag  | file:line  | fun:line | */
-#define LOG_VERBOSE_FORMAT "%s%s | %-5s | %-15s:%d | %s:%d | "
+#define LOG_VERBOSE_FORMAT "%s%s | %-5s | %-15s:%d | %s:%d | "        ///< Verbose format of logs, default to all logs with levels higher than info.
                      /*color,time| tag  | fun:line | */
-#define LOG_STRICT_FORMAT  "%s%s | %-5s | %s:%d | "
+#define LOG_STRICT_FORMAT  "%s%s | %-5s | %s:%d | "                   ///< Strict format of logs, default to verbose, debug and info logs.
 
 #ifndef LOG_TIME_FORMAT
-#define LOG_TIME_FORMAT "%Y-%m-%d %H:%M:%S"
+#define LOG_TIME_FORMAT "%Y-%m-%d %H:%M:%S"                           ///< Time format used by all logs.
 #endif
 
 #ifndef SET_LOG_LEVEL
-#define SET_LOG_LEVEL LOG_LEVEL_DEBUG
+#define SET_LOG_LEVEL LOG_LEVEL_DEBUG                                 ///< Default log level is set to debug, change by defining SET_LOG_LEVEL level macro.
 #endif
 
 void logger_set_log_output(FILE* file);
@@ -51,7 +60,7 @@ int logger_get_lock(void);
 
 void logger_create_semphr(void);
 
-void logger_return_lock(void);
+int logger_return_lock(void);
 
 int set_logging_to_socket(char* address, uint16_t port);
 
@@ -73,19 +82,23 @@ static inline char *timenow() {
 }
 
 #if SET_LOG_LEVEL >= LOG_LEVEL_FATAL
-#define LOG_FATAL(...)     logger_write(LOG_LEVEL_FATAL, LOG_VERBOSE_FORMAT, RED_BOLD_UNTERLINE, timenow(), FATAL_TAG, __FILE__, __LINE__, __func__, __LINE__); \
+#define LOG_FATAL(...)     logger_get_lock();        \
+                           logger_write(LOG_LEVEL_FATAL, LOG_VERBOSE_FORMAT, RED_BOLD_UNTERLINE, timenow(), FATAL_TAG, __FILE__, __LINE__, __func__, __LINE__); \
                            logger_write(LOG_LEVEL_FATAL, __VA_ARGS__);   \
                            logger_write(LOG_LEVEL_FATAL, "%s\n", RESET); \
-                           logger_flush_logs()
+                           logger_flush_logs();      \
+                           logger_return_lock()
 #else
 #define LOG_FATAL(...)
 #endif
 
 #if SET_LOG_LEVEL >= LOG_LEVEL_ERROR
-#define LOG_ERROR(...)     logger_write(LOG_LEVEL_ERROR, LOG_VERBOSE_FORMAT, RED, timenow(), ERROR_TAG, __FILE__, __LINE__, __func__, __LINE__);   \
+#define LOG_ERROR(...)     logger_get_lock();        \
+                           logger_write(LOG_LEVEL_ERROR, LOG_VERBOSE_FORMAT, RED, timenow(), ERROR_TAG, __FILE__, __LINE__, __func__, __LINE__);   \
                            logger_write(LOG_LEVEL_ERROR, __VA_ARGS__);   \
                            logger_write(LOG_LEVEL_ERROR, "%s\n", RESET); \
-                           logger_flush_logs()
+                           logger_flush_logs();       \
+                           logger_return_lock()
 #else
 #define LOG_ERROR(...)
 #endif
@@ -115,7 +128,7 @@ static inline char *timenow() {
                               logger_write(LOG_LEVEL_INFO, __VA_ARGS__);   \
                               logger_write(LOG_LEVEL_INFO, "%s\n", RESET); \
                               logger_flush_logs();
-                              logger_return_lock();
+                              logger_return_lock()
   #endif
 #else
 #define LOG_INFO(...)
@@ -123,15 +136,19 @@ static inline char *timenow() {
 
 #if SET_LOG_LEVEL >= LOG_LEVEL_DEBUG
   #ifndef USE_VERBOSE_FORMAT
-    #define LOG_DEBUG(...)     logger_write(LOG_LEVEL_DEBUG, LOG_STRICT_FORMAT, RESET, timenow(), DEBUG_TAG, __func__, __LINE__); \
+    #define LOG_DEBUG(...)     logger_get_lock();      \
+                               logger_write(LOG_LEVEL_DEBUG, LOG_STRICT_FORMAT, RESET, timenow(), DEBUG_TAG, __func__, __LINE__); \
                                logger_write(LOG_LEVEL_DEBUG, __VA_ARGS__);   \
                                logger_write(LOG_LEVEL_DEBUG, "%s\n", RESET); \
-                               logger_flush_logs()
+                               logger_flush_logs();    \
+                               logger_return_lock();
     #else
-    #define LOG_DEBUG(...)     logger_write(LOG_LEVEL_DEBUG, LOG_VERBOSE_FORMAT, RESET, timenow(), DEBUG_TAG, __FILE__, __LINE__, __func__, __LINE__); \
+    #define LOG_DEBUG(...)     logger_get_lock();      \
+                               logger_write(LOG_LEVEL_DEBUG, LOG_VERBOSE_FORMAT, RESET, timenow(), DEBUG_TAG, __FILE__, __LINE__, __func__, __LINE__); \
                                logger_write(LOG_LEVEL_DEBUG, __VA_ARGS__);   \
                                logger_write(LOG_LEVEL_DEBUG, "%s\n", RESET); \
-                               logger_flush_logs()
+                               logger_flush_logs();     \
+                               logger_return_lock()
   #endif
 #else
 #define LOG_DEBUG(...)
