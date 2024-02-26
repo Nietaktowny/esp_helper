37093b5dcab14be7cccb3dc60d9d2fd938ef4d94
Nietaktowny <wojciech.lukasz.mytych@gmail.com>
2024-02-26 17:43:12 +0100

    Add checking for internet connection in cli_manager, docs in cli manager

--- projects/lib_tests/lib/cli_manager/include/cli_manager.h
+++ projects/lib_tests/lib/cli_manager/include/cli_manager.h
@@ -1,17 +1,34 @@
 #include "inttypes.h"
 
-#define CLI_PORT                        27015
-#define CLI_KEEPALIVE_IDLE              5               ///< TCP keep-alive idle time(s).
-#define CLI_KEEPALIVE_INTERVAL          5               ///< TCP keep-alive interval time(s).
-#define CLI_KEEPALIVE_COUNT             3               ///< TCP keep-alive packet retry send counts.
-#define CLI_CMD_INTS_NUMBER             2               ///< Number of ints used to indicate command in received data.
-#define CLI_CMD_ARGS_NUMBER             64              ///< Number of chars used to indicate arguments in received data.
-#define CLI_CMD_MAX_COMMANDS            64              ///< Max number of commands to register.
+#define CLI_PORT                        27015                   ///< TCP Cli manager listen port.
+#define CLI_KEEPALIVE_IDLE              5                       ///< TCP keep-alive idle time(s).
+#define CLI_KEEPALIVE_INTERVAL          5                       ///< TCP keep-alive interval time(s).
+#define CLI_KEEPALIVE_COUNT             3                       ///< TCP keep-alive packet retry send counts.
+#define CLI_CMD_INTS_NUMBER             2                       ///< Number of ints used to indicate command in received data.
+#define CLI_CMD_ARGS_NUMBER             64                      ///< Number of chars used to indicate arguments in received data.
+#define CLI_CMD_MAX_COMMANDS            64                      ///< Max number of commands to register.
 
-#define CLI_ERR_BASE                    0x0500
-#define CLI_ERR_CONNECT_FAIL            CLI_ERR_BASE + 0x0001
+#define CLI_ERR_BASE                    0x0500                  
+#define CLI_ERR_CONNECT_FAIL            0x0001 + CLI_ERR_BASE   ///< Error when trying to connect to remote host.
+#define CLI_ERR_NO_INTERNET             0x0002 + CLI_ERR_BASE   ///< Device has no internet connection.
 
-
+/**
+ * @brief Log all logger library logs to remote host on TCP port.
+ * 
+ * @attention Ports under 1000 cannot be used, as they are restricted.
+ * 
+ * @param port Port on which set up TCP server.
+ * @param address Address on which set up TCP server.
+ * 
+ * @retval ERR_NULL_POINTER If address is NULL.
+ * @retval CLI_ERR_CONNECT_FAIL If error occurs during setup of TCP server.
+ * @retval CLI_ERR_NO_INTERNET If device has no internet connection.
+ */
 int cli_set_remote_logging(uint16_t port, const char* address);
 
+/**
+ * @brief Task responsible for accepting new hosts in TCP server.
+ * 
+ * @param args Additional arguments passed to task.
+ */
 void cli_accept_task(void* args);
