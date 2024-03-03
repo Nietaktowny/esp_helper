75046e265a86bf4b29a02df3511d3d4454fb0285
Nietaktowny <Nietaktowny@users.noreply.github.com>
2024-03-03 00:40:03 +0000

    deploy: dd6df1208f58933b9d8755f90b2c365da948277a

--- projects/lib_tests/build_all.py
+++ projects/lib_tests/build_all.py
@@ -1,13 +1,22 @@
 import os
 import subprocess
 
+print('\x1b[1;32m' + 'Building all pytest projects...' + '\x1b[0m')
+
 for file in os.listdir():
     if file.startswith("test_"):
-        print('Building test project directory: ' + file)
+        print('\x1b[0;34m' + 'Building test project: ' + file + '\x1b[0m')
         os.chdir(file)
         subprocess.run(["idf.py", "build"])
-        if(os.path.isdir("test")):
-            os.chdir("test")
-            print(" Building test directory of project: " + os.getcwd())
-            os.chdir("..")
         os.chdir("..")
+        
+print('\x1b[1;32m' + 'Building all qemu Unity tests...' + '\x1b[0m')
+start_path = os.getcwd()
+for file in os.listdir():
+    os.chdir(start_path)
+    if file.startswith("test_"):
+        os.chdir(file)
+        if(os.path.isdir('test')):
+            os.chdir("test")
+            print('\x1b[0;34m' + 'Building qemu tests on path: ' + file + "/test" + '\x1b[0m')
+            subprocess.run(["idf.py", "build"])
