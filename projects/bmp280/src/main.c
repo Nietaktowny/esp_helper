15ccf98eb1105ef7fa5066230a13de6739fe6772
Nietaktowny <Nietaktowny@users.noreply.github.com>
2024-03-17 00:05:30 +0000

    deploy: 39c5f5c084661902f16fdd0ec06a693b8f2cd996

--- projects/bmp280/src/main.c
+++ projects/bmp280/src/main.c
@@ -13,12 +13,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#ifndef ESP32_C3_SUPERMINI
-#include "cli_manager.h"
 
-#include "esp_netif_sntp.h"
+#include "cli_manager.h"
 #include <driver/gpio.h>
-#endif
+
 
 #define MY_SSID "TP-LINK_AD8313"
 #define MY_PSK "20232887"
@@ -43,6 +41,7 @@ void read_temperature_task(void *args)
     err_c_t err = 0;
     bmp_handle_t bmp = NULL;
     i2c_c_bus_handle_t bus = (i2c_c_bus_handle_t)args;
+    http_client_t client = NULL;
     float temperature = 0;
     float pressure = 0;
     float altitude = 0;
@@ -53,9 +52,7 @@ void read_temperature_task(void *args)
         .standby = BMP_STANDBY_1000M,
         .mode = BMP_MODE_NORMAL,
     };
-    char post_data[256];
-    memutil_zero_memory(&post_data, sizeof(post_data));
-
+    char post_data[256] = {0};
     char device_id[20] = {0};
     sysutil_get_chip_base_mac_as_str(device_id, sizeof(device_id));
 
@@ -69,9 +66,16 @@ void read_temperature_task(void *args)
     if (err != ERR_C_OK)
     {
         LOG_ERROR("cannot configure BMP280 device");
-        // vTaskDelete(NULL);
+        vTaskDelete(NULL);
     }
     LOG_INFO("Identified BMP280 device:\n address: %x\n chip id: %x\n mode: %x", bmp_get_i2c_addr(bmp), bmp_get_chip_id(bmp), bmp_get_mode(bmp));
+    
+    err = http_client_init_reuse(&client, "wmytych.usermd.net", "modules/setters/insert_data.php");
+    if(err != ERR_C_OK) {
+        LOG_ERROR("error %d when trying to prepare http_client handle for reuse: %s", err, error_to_name(err));
+        vTaskDelete(NULL);
+    }
+
     while (1)
     {
         bmp_readoutFloat(bmp, &temperature, &pressure);
@@ -80,38 +84,62 @@ void read_temperature_task(void *args)
         LOG_INFO("pressure: %.2fhPa", pressure * 0.01);
         LOG_INFO("altitude: %.2f m. n. p. m.", altitude);
         sprintf(post_data, "device_id=%s&temperature=%.2f&pressure=%.2f&altitude=%.2f", device_id, temperature, pressure * 0.01, altitude);
-        LOG_DEBUG("data to send to database: %s", post_data);
-        err = http_client_post("wmytych.usermd.net", "modules/setters/insert_data.php", post_data, HTTP_CLIENT_POST_USE_STRLEN);
-        LOG_INFO("Client POST request returned: %d", err);
+        LOG_VERBOSE("data to send to database: %s", post_data);
+        err = http_client_post_reuse(client, post_data, HTTP_CLIENT_POST_USE_STRLEN);
+        LOG_VERBOSE("Client POST request returned: %d", err);
         vTaskDelay(pdMS_TO_TICKS(30000));
     }
+
+    http_client_deinit_reuse(&client);
+    //if ever task got here, delete it
+    vTaskDelete(NULL);
 }
 
-void inspect_task(void *args)
+void update_wifi_info_task(void *args)
 {
-    char *wifi_c_info = NULL;
-    NEW_SIZE(wifi_c_info, 300); // TODO add constant to wifi_controller, something like WIFI_C_STATUS_JSON_MIN_BUFLEN
-
-    char *device_info = NULL;
-    NEW_SIZE(device_info, 350);
-
+    err_c_t err = ERR_C_OK;
+    char wifi_c_info[300] = {0};
+    char device_info[350] = {0};
     char device_id[20] = {0};
+
     sysutil_get_chip_base_mac_as_str(device_id, sizeof(device_id));
 
     while (1)
     {
+
+        err = wifi_c_get_status_as_json(wifi_c_info, 300);
+
+        snprintf(device_info, 350, "device_id=%s&wifi_info=%s", device_id, wifi_c_info);
+        err = http_client_post("wmytych.usermd.net", "modules/setters/update_wifi_info.php", device_info, HTTP_CLIENT_POST_USE_STRLEN);
+        if (err != ERR_C_OK)
+        {
+            LOG_ERROR("error %d when posting device info data: %s", error_to_name(err));
+            memutil_zero_memory(wifi_c_info, sizeof(wifi_c_info));
+            memutil_zero_memory(device_info, sizeof(device_info));
+            vTaskDelay(pdMS_TO_TICKS(60000 * 3));
+            continue;
+        }
+
+        LOG_VERBOSE("sent wifi_c_status to the server");
+        vTaskDelay(pdMS_TO_TICKS(60000 * 3)); /** @todo Make it a constant or configuration variable? */
+    }
+}
+
+void inspect_heap_task(void *args)
+{
+    while (1)
+    {
         uint32_t free_heap = esp_get_free_heap_size();
         uint32_t ever_free_heap = esp_get_minimum_free_heap_size();
-        wifi_c_get_status_as_json(wifi_c_info, 300);
-        snprintf(device_info, 350, "device_id=%s&wifi_info=%s", device_id, wifi_c_info);
-        http_client_post("wmytych.usermd.net", "modules/setters/update_wifi_info.php", device_info, HTTP_CLIENT_POST_USE_STRLEN);
-        LOG_DEBUG("sent wifi_c_status to the server");
         LOG_DEBUG("Currently available heap: %lu", free_heap);
         LOG_DEBUG("The minimum heap size that was ever available: %lu", ever_free_heap);
-        vTaskDelay(pdMS_TO_TICKS(60000 * 3)); /** @todo Make it a constant or configuration variable?*/
+
+        if(free_heap < 8000) {
+            LOG_ERROR("Currently free heap very low, restarting...");
+            esp_restart();
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
-    DELETE(wifi_c_info);
-    DELETE(device_info);
 }
 
 void on_connect_handler(void)
@@ -123,10 +151,14 @@ void on_connect_handler(void)
 #endif
 
     helper_perform_ota();
+
+    sysutil_setup_ntp_server("pool.ntp.org", 1);
+
     i2c_c_bus_handle_t i2c_bus = NULL;
     i2c_c_init_bus(I2C_C_NUM_0, BUS_GPIO_SCL, BUS_GPIO_SDA, &i2c_bus);
+    xTaskCreate(inspect_heap_task, "inspect_task", 1024 * 2, NULL, 4, NULL);
     xTaskCreate(read_temperature_task, "log_task", 8096, (void *)i2c_bus, 1, NULL);
-    xTaskCreate(inspect_task, "inspect_heap_task", 6 * 1024, NULL, 2, NULL);
+    xTaskCreate(update_wifi_info_task, "inspect_heap_task", 1024 * 6, NULL, 3, NULL);
 }
 
 #ifdef ESP_WROVER_KIT
@@ -167,9 +199,6 @@ void app_main()
     wifi_c_sta_register_connect_handler(on_connect_handler);
 
     wifi_manager_init();
-
-#ifndef ESP32_C3_SUPERMINI
-	sysutil_setup_ntp_server("pool.ntp.org", 1);
+	
 	cli_set_remote_logging(27015, wifi_c_get_sta_ipv4());
-#endif
 }
